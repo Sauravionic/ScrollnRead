68b22acbef56378ff101d36d9970f997813351e6
Saurav <srivastava2000saurav@gmail.com>
2021-11-18 22:16:33 +0530

    first commit

---
+++ src/main/java/com/ionic/ScrollnRead/ScrollnReadApplication.java
@@ -0,0 +1,13 @@
+package com.ionic.ScrollnRead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrollnReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrollnReadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ionic/ScrollnRead/ScrollnReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ionic.ScrollnRead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrollnReadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
